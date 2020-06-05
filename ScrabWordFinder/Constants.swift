a67d1791a714942eb04814cabc085bf2d00fee66
piotr <pdobrzynski>
2020-06-05 12:25:39 +0200

    -Added Pods file -Added Constants and Resources to project

---
+++ ScrabWordFinder/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  ScrabWordFinder
+//
+//  Created by Piotr on 04/06/2020.
+//  Copyright © 2020 Piotr. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+
+    static let mainToResultSegue = "MainToResult"
+
+}
